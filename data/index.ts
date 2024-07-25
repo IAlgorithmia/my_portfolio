baf40e4fc93b195aac5ca514a9146b3b312be1a6
IAlgorithmia <peterudeskey@gmail.com>
2024-07-25 12:58:48 +0530

    Hey

--- data/index.ts
+++ data/index.ts
@@ -49,12 +49,12 @@ export const gridItems = [
 
     {
         id: 5,
-        title: "Currently pushing my Competitive Programming to Perfection",
+        title: 'Currently pushing my Competitive Programming to Perfection',
         description: 'The Inside Scoop',
         className: 'md:col-span-3 md:row-span-2',
         imgClassName: 'absolute right-0 bottom-0 md:w-96 w-60',
         titleClassName: 'justify-center md:justify-start lg:justify-center',
-        img: '/public/b5.svg',
+        img: './public/b5.svg',
         spareImg: '/grid.svg',
     },
     {
